08661ca0d274db58e71018e82ab79238871b1fe4
sousair <sousair2001@gmail.com>
2024-03-01 15:31:25 -0300

    feat: fix filejump shortcuts

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -6,6 +6,7 @@ vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
 vim.keymap.set("n", "<C-e>", function () harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 
-vim.keymap.set("n", "<C-u>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-i>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-o>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
